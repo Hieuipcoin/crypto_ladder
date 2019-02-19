b919684478b9ef3bcff5b1256e92e94d2a28c190
Riki <hieult@ipcoin.vn>
2019-02-19 17:36:40 +0700

    simple API call (ping) to binance exchange

--- lib/crypto_ladder.ex
+++ lib/crypto_ladder.ex
@@ -1,18 +1,71 @@
 defmodule CryptoLadder do
-  @moduledoc """
-  Documentation for CryptoLadder.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  @endpoint "https://api.binance.com"
 
-  ## Examples
-
-      iex> CryptoLadder.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start do
+    GenServer.start(__MODULE__, nil)
   end
+
+  def cast(crypto_ladder) do
+    GenServer.cast(crypto_ladder, {:cast, "from cast"})
+  end
+
+  def ping_binance(pid) do
+    GenServer.call(pid, :ping_binance)
+  end
+
+  @impl true
+  def init(_) do
+    IO.puts("from init")
+    {:ok, "from init"}
+  end
+
+  defp get_binance(url, headers \\ []) do
+    HTTPoison.get("#{@endpoint}#{url}", headers)
+    |> parse_get_response
+  end
+
+  defp parse_get_response({:ok, response}) do
+    response.body
+    |> Poison.decode()
+    |> parse_response_body
+  end
+
+  defp parse_get_response({:error, err}) do
+    {:error, {:http_error, err}}
+  end
+
+  defp parse_response_body({:ok, data}) do
+    case data do
+      %{"code" => _c, "msg" => _m} = error -> {:error, error}
+      _ -> {:ok, data}
+    end
+  end
+
+  defp parse_response_body({:error, err}) do
+    {:error, {:poison_decode_error, err}}
+  end
+
+  @impl GenServer
+  def handle_cast({:cast, string_cast}, _state) do
+    IO.puts("riki handle cast: #{string_cast}")
+    {:noreply, ""}
+  end
+
+  @impl GenServer
+  def handle_call(:ping_binance, _, state) do
+    result = get_binance("/api/v1/ping")
+
+    {
+      :reply,
+      result,
+      state
+    }
+  end
+
+
+
+
+
 end
