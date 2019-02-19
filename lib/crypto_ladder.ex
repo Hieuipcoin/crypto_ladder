74d26263f3c3b48d5cd0549a0eb9bbebfa872427
Riki <hieult@ipcoin.vn>
2019-02-19 09:32:09 +0700

    first commit

---
+++ lib/crypto_ladder.ex
@@ -0,0 +1,18 @@
+defmodule CryptoLadder do
+  @moduledoc """
+  Documentation for CryptoLadder.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CryptoLadder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/crypto_ladder_test.exs
@@ -0,0 +1,8 @@
+defmodule CryptoLadderTest do
+  use ExUnit.Case
+  doctest CryptoLadder
+
+  test "greets the world" do
+    assert CryptoLadder.hello() == :world
+  end
+end
